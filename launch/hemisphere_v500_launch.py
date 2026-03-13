bcc9e5dbfb22b8a5ca38948035ae296b399a606c
Aarush Jain <aarush@polymathrobotics.com>
2026-03-13 18:20:41 +0000

    added launch file

--- launch/hemisphere_v500_launch.py
+++ launch/hemisphere_v500_launch.py
@@ -6,15 +6,13 @@ from launch_ros.descriptions import ComposableNode
 from launch.actions import ExecuteProcess, TimerAction
 
 def generate_launch_description():
-    # 1. Locate the config file from the share directory
-    # Note: Make sure your config file is named 'config.yaml' inside the 'config' folder
+
     config_path = os.path.join(
         get_package_share_directory('hemisphere_gnss_v500_driver'),
         'config',
         'config.yaml'
     )
 
-    # 2. Define the Driver Component
     hemisphere_node = ComposableNode(
         package='hemisphere_gnss_v500_driver',
         plugin='hemisphere_gnss_v500_driver::HemisphereDriverNode',
@@ -22,7 +20,6 @@ def generate_launch_description():
         parameters=[config_path]
     )
 
-    # 3. Create the Container to host the component
     container = ComposableNodeContainer(
         name='hemisphere_container',
         namespace='',
@@ -32,16 +29,11 @@ def generate_launch_description():
         output='screen',
     )
 
-    # 4. Automate Lifecycle: Configure
-    # This moves the node from Unconfigured -> Inactive
     configure_cmd = ExecuteProcess(
         cmd=['ros2', 'lifecycle', 'set', '/hemisphere_driver', 'configure'],
         output='screen'
     )
 
-    # 5. Automate Lifecycle: Activate
-    # This moves the node from Inactive -> Active.
-    # We wait 2 seconds to ensure 'configure' has fully completed.
     activate_cmd = TimerAction(
         period=2.0,
         actions=[
